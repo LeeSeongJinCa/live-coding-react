b297b1fcf18783818739b0e78ca1f087a8899305
LeeSeongJin <leeseongjinca@pixo.co>
2025-02-07 22:39:10 +0900

    Add: 문제 2번 풀이

---
+++ src/components/2.Implement-a-Toggle-Switch.tsx
@@ -0,0 +1,25 @@
+/**
+ * 2. Implement a Toggle Switch
+ *
+ * Problem:
+ * - Create a toggle switch component between "On" and "Off" states.
+ * - '켜기'와 '끄기' 상태 사이의 토글 스위치 컴포넌트를 만듭니다.
+ */
+
+import { useState } from 'react';
+
+export const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggle = () => setIsOn((prev) => !prev);
+
+  return (
+    <div>
+      <h1>Toggle Switch: {isOn ? 'On' : 'Off'}</h1>
+
+      <button type="button" onClick={toggle}>
+        Toggle
+      </button>
+    </div>
+  );
+};
--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,9 +1,15 @@
 import { Counter } from '../components/1.Create-a-Counter-Component';
+import { ToggleSwitch } from '../components/2.Implement-a-Toggle-Switch';
 
 export const ROUTES = [
   {
     path: '/1',
     name: '1. Create a Counter Component',
     component: Counter,
+  },
+  {
+    path: '/2',
+    name: '2. Implement a Toggle Switch',
+    component: ToggleSwitch,
   },
 ];
