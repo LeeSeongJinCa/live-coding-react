3c42598efcd59c4811e305114ff107d8d11e6657
LeeSeongJin <leeseongjinca@pixo.co>
2025-02-07 22:36:41 +0900

    Add: 문제 1번 풀이

---
+++ src/components/1.Create-a-Counter-Component.tsx
@@ -0,0 +1,27 @@
+/**
+ * 1. Create a Counter Component.
+ * 1. 카운터 컴포넌트를 생성합니다.
+ *
+ * Problem
+ * - Create a simple counter component that increases or decreases the count when clicking buttons.
+ * - 버튼을 클릭할 때 카운트를 늘리거나 줄이는 간단한 카운터 컴포넌트를 만듭니다.
+ */
+
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+
+      <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+        1 증가
+      </button>
+      <button type="button" onClick={() => setCount((prev) => prev - 1)}>
+        1 감소
+      </button>
+    </div>
+  );
+};
--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,7 +1,9 @@
+import { Counter } from '../components/1.Create-a-Counter-Component';
+
 export const ROUTES = [
-  // {
-  //   path: '/1',
-  //   name: '1. Create a Counter Component',
-  //   component: Counter,
-  // },
+  {
+    path: '/1',
+    name: '1. Create a Counter Component',
+    component: Counter,
+  },
 ];
