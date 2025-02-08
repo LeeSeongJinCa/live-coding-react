f900a858f9a1483615c92d8a43dbf6ce16317cf4
LeeSeongJin <leeseongjinca@pixo.co>
2025-02-08 11:12:54 +0900

    Setting: react-router v7 설정
    - react 버전을 버전 호환성에 맞게 18 버전으로 업그레이드함

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,21 @@
 import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import App from './App';
 import { ROUTES } from './shared/routes';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: ROUTES.map((route) => ({
-      path: route.path,
-      Component: route.component,
-    })),
-  },
-]);
+const root = document.getElementById('root') as HTMLElement;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById('root'),
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {ROUTES.map((route) => (
+          <Route key={route.path} path={route.path} Component={route.component} />
+        ))}
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 );
