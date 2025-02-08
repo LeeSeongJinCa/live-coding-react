4a0933e812db0ab32b5538e118597c1968c1554a
LeeSeongJin <leeseongjinca@pixo.co>
2025-02-08 13:32:32 +0900

    Design: 네비게이션 디자인 개발
    - 시멘틱 태그로 네비게이션 및 메인 영역 지정

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,33 @@ import { ROUTES } from './shared/routes';
 
 function App() {
   return (
-    <div className="App">
-      <ul>
-        <li key="/">
-          <a href="/">Home</a>
-        </li>
-        {ROUTES.map((route) => (
-          <li key={route.path}>
-            <a href={route.path}>{route.name}</a>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        columnGap: 12,
+      }}
+    >
+      <nav
+        style={{
+          width: 240,
+        }}
+      >
+        <ul>
+          <li key="/">
+            <a href="/">Home</a>
           </li>
-        ))}
-      </ul>
+          {ROUTES.map((route) => (
+            <li key={route.path}>
+              <a href={route.path}>{route.name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
 
-      <Outlet />
+      <main style={{ flex: 1 }}>
+        <Outlet />
+      </main>
     </div>
   );
 }
