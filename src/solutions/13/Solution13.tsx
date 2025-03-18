0b287044282561006ff60a49373728afa50e4aa1
LeeSeongJin <leeseongjinca@pixo.co>
2025-03-18 13:26:51 +0900

    Feat: 가상 스크롤 컴포넌트에 scrollToIndex 기능 추가

--- src/solutions/13/Solution13.tsx
+++ src/solutions/13/Solution13.tsx
@@ -6,14 +6,16 @@
  * - 뷰포트 내에 표시되는 항목만 렌더링하여 큰 항목 목록을 효율적으로 렌더링하는 가상화된 목록 컴포넌트를 만듭니다.
  */
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-import { VirtualizedList } from './VirtualizedList';
+import { VirtualizedList, VirtualizedListRef } from './VirtualizedList';
 
 export const Solution13 = () => {
-  const items = Array.from({ length: 100 }, (_, index) => ({ id: index + 1 }));
-  const itemHeight = 100;
-  const containerHeight = 100 * 3;
+  const ref = useRef<VirtualizedListRef>(null);
+
+  const items = Array.from({ length: 1000 }, (_, index) => ({ id: index + 1 }));
+  const itemHeight = 50;
+  const containerHeight = itemHeight * 8;
 
   return (
     <div
@@ -22,11 +24,21 @@ export const Solution13 = () => {
       }}
     >
       <VirtualizedList
+        ref={ref}
         itemCount={items.length}
         itemHeight={itemHeight}
         containerHeight={containerHeight}
         renderItem={({ index }) => <Item index={index} />}
+        overscanCount={3}
       />
+      <button
+        type="button"
+        onClick={() => {
+          ref.current?.scrollToIndex(4);
+        }}
+      >
+        Scroll to index 4
+      </button>
     </div>
   );
 };
--- src/solutions/13/VirtualizedList.tsx
+++ src/solutions/13/VirtualizedList.tsx
@@ -1,76 +1,112 @@
-import { ReactNode, UIEventHandler, useCallback, useState } from 'react';
+import {
+  forwardRef,
+  ReactNode,
+  UIEventHandler,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 
-type IVirtualizedListProps = {
+export interface VirtualizedListRef {
+  scrollToIndex: (index: number) => void;
+}
+
+export interface VirtualizedListProps {
   itemCount: number;
   itemHeight: number;
   containerHeight: number;
   renderItem: ({ index }: { index: number }) => ReactNode;
   // Optional
   overscanCount?: number;
-};
+}
 
-export const VirtualizedList = ({
-  itemCount,
-  itemHeight,
-  containerHeight,
-  renderItem,
-  //
-  overscanCount = 3,
-}: IVirtualizedListProps) => {
-  const totalHeight = itemCount * itemHeight;
+export const VirtualizedList = forwardRef<VirtualizedListRef, VirtualizedListProps>(
+  (
+    {
+      itemCount,
+      itemHeight,
+      containerHeight,
+      renderItem,
+      //
+      overscanCount = 3,
+    },
+    ref,
+  ) => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    useImperativeHandle(ref, () => {
+      return {
+        scrollToIndex: (index: number) => {
+          const scrollTop = index * itemHeight;
+          containerRef.current?.scrollTo({ top: scrollTop });
+        },
+      };
+    });
 
-  const [startIndex, setStartIndex] = useState<number>(0);
-  const endIndex = startIndex + Math.floor(containerHeight / itemHeight);
+    const totalHeight = itemCount * itemHeight;
 
-  const handleScroll = useCallback<UIEventHandler<HTMLDivElement>>((event) => {
-    // 0 ~ (totalHeight - containerHeight) 사이의 값
-    const scrollTop = event.currentTarget.scrollTop;
+    const [startIndex, setStartIndex] = useState<number>(0);
+    const endIndex = startIndex + (Math.floor(containerHeight / itemHeight) - 1); // index이므로 1 빼줌
 
-    const index = Math.floor(scrollTop / itemHeight);
-    const newStartIndex = index;
+    const handleScroll = useCallback<UIEventHandler<HTMLDivElement>>((event) => {
+      // 0 ~ (totalHeight - containerHeight) 사이의 값
+      const scrollTop = event.currentTarget.scrollTop;
 
-    if (newStartIndex !== startIndex) {
-      setStartIndex(newStartIndex);
-    }
-  }, []);
+      const index = Math.floor(scrollTop / itemHeight);
+      const newStartIndex = index;
 
-  return (
-    <div
-      style={{
-        position: 'relative',
-        width: '100%',
-        height: containerHeight,
-        overflow: 'hidden',
-      }}
-    >
+      if (newStartIndex !== startIndex) {
+        setStartIndex(newStartIndex);
+      }
+    }, []);
+
+    return (
       <div
-        style={{ height: '100%', maxHeight: '100%', overflow: 'auto' }}
-        onScroll={handleScroll}
+        className="VirtualizedList-root"
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: containerHeight,
+          overflow: 'hidden',
+        }}
       >
-        <div style={{ position: 'relative', height: totalHeight }}>
-          {Array.from({ length: itemCount }).map((_, index) => {
-            if (index < startIndex - overscanCount) return null;
-            if (index > endIndex + overscanCount) return null;
+        <div
+          ref={containerRef}
+          className="VirtualizedList-scrollable"
+          style={{ height: '100%', maxHeight: '100%', overflow: 'auto' }}
+          onScroll={handleScroll}
+        >
+          <div
+            className="VirtualizedList-content"
+            style={{ position: 'relative', height: totalHeight }}
+          >
+            {Array.from({ length: itemCount }).map((_, index) => {
+              if (index < startIndex - overscanCount) return null;
+              if (index > endIndex + overscanCount) return null;
 
-            return (
-              <div
-                key={index}
-                data-index={index}
-                style={{
-                  position: 'absolute',
-                  top: index * itemHeight,
-                  left: 0,
-                  width: '100%',
-                  height: itemHeight,
-                  backgroundColor: index % 2 === 0 ? '#7e7e7e' : '#a5a5a5',
-                }}
-              >
-                {renderItem({ index })}
-              </div>
-            );
-          })}
+              return (
+                <div
+                  key={index}
+                  data-index={index}
+                  className="VirtualizedList-item"
+                  style={{
+                    position: 'absolute',
+                    top: index * itemHeight,
+                    left: 0,
+                    width: '100%',
+                    height: itemHeight,
+                    backgroundColor: index % 2 === 0 ? '#7e7e7e' : '#a5a5a5',
+                  }}
+                >
+                  {renderItem({ index })}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
+
+VirtualizedList.displayName = 'VirtualizedList';
