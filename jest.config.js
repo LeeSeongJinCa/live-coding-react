c8994c49eaf2cec8ff5cff79a743e3b0c824b0b6
LeeSeongJin <leeseongjinca@pixo.co>
2025-03-20 00:02:24 +0900

    Chore: jest config 파일의 파일 확장자 명확하게 수정

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,21 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testMatch: ['**/*.test.ts(x)'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['**/*.test.+(ts|tsx)'],
+
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/.next/',
+  ],
+
   transform: {
-    '^.+.ts(x)?$': ['ts-jest', {}],
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        diagnostics: true, // 타입스크립트 진단 활성화
+      },
+    ],
   },
 };
